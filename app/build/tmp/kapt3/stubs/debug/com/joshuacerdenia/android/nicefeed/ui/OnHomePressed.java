56e35f4e3cae836a96ff6cb71e0cb55fee861177
Aile <riwgal2@gmail.com>
2021-03-31 18:54:41 +0200

    🔥 remove

--- app/build/tmp/kapt3/stubs/debug/com/joshuacerdenia/android/nicefeed/ui/OnHomePressed.java
+++
@@ -1,9 +0,0 @@
-package com.joshuacerdenia.android.nicefeed.ui;
-
-import java.lang.System;
-
-@kotlin.Metadata(mv = {1, 4, 2}, bv = {1, 0, 3}, k = 1, d1 = {"\u0000\u0010\n\u0002\u0018\u0002\n\u0002\u0010\u0000\n\u0000\n\u0002\u0010\u0002\n\u0000\bf\u0018\u00002\u00020\u0001J\b\u0010\u0002\u001a\u00020\u0003H&\u00a8\u0006\u0004"}, d2 = {"Lcom/joshuacerdenia/android/nicefeed/ui/OnHomePressed;", "", "onHomePressed", "", "app_debug"})
-public abstract interface OnHomePressed {
-    
-    public abstract void onHomePressed();
-}
